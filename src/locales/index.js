3c3be323ea8751b25830edb6799dcdd3aec91f6a
Dmitry Losiev <d.losiev@andersenlab.com>
2020-07-17 20:58:43 +0300

    added locales

---
+++ src/locales/index.js
@@ -0,0 +1,23 @@
+const locale = {
+  projectButtons: {
+    create: 'Create',
+    edit: 'Edit',
+    delete: 'Delete',
+    cancel: 'Cancel',
+    next: 'Next',
+    prev: 'Prev',
+    plus: '+',
+  },
+  formFields: {
+    formTitle: 'Add/edit medicine',
+    code: 'Code',
+    name: 'Name',
+    price: 'Price',
+    expiration: 'Expiration date',
+    indication: 'Indication',
+    contraindication: 'Contraindication',
+    composition: 'Composition and releases form',
+  },
+};
+
+export { locale };
