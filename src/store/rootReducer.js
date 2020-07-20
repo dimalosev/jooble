4e4fd80936c5efd99da2bdd045ad90e068bba50a
Dmitry Losiev <d.losiev@andersenlab.com>
2020-07-20 09:17:31 +0300

    instaled redux-form for validdation

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
+import { reducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   firestore: firestoreReducer,
+  form: reducer,
 });
 
 export { rootReducer };
