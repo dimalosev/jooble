03ca71b597f694f3f265d7a4cc13052000f0eb9d
Dmitry Losiev <d.losiev@andersenlab.com>
2020-07-20 09:18:52 +0300

    added validation errors

--- src/locales/index.js
+++ src/locales/index.js
@@ -19,6 +19,19 @@ const locale = {
     contraindication: 'Contraindication',
     composition: 'Composition and releases form',
   },
+  errors: {
+    required: 'Required',
+    maxLetters: 'Max 2000 letters',
+    minLettersCode: 'Min 5 letters',
+    maxLettersCode: 'Max 10 letters',
+    minLettersName: 'Min 5 letters',
+    maxLettersName: 'Max 100 letters',
+    number: 'Must be a number',
+    minNumberShelfLife: 'Min number 1',
+    maxNumberShelfLife: 'Max number 1000',
+    minNumberPrice: 'Min number 0.01',
+    maxNumberPrice: 'Max number 1000000',
+  },
 };
 
 export { locale };
